140ce93bd4f5d02f3d95f5bf678dca7c0c2ab81e
yeasinmiah1272001 <137316520+yeasinmiah1272001@users.noreply.github.com>
2025-04-13 19:33:19 +0600

    solve

--- src/app/api/todos/route.ts
+++ src/app/api/todos/route.ts
@@ -1,6 +1,7 @@
 import { NextResponse } from "next/server";
 import dbConnect from "@/lib/dbConnect";
 import Todo from "@/models/Todo";
+import { request } from "node:http";
 
 export async function GET() {
   await dbConnect();
@@ -14,3 +15,25 @@ export async function POST(request: Request) {
   const newTodo = await Todo.create({ title });
   return NextResponse.json(newTodo);
 }
+
+export const DELETE = async (request: Request) => {
+  await dbConnect();
+  const { id } = await request.json(); // request body থেকে id নেওয়া হচ্ছে
+
+  try {
+    const deletedTodo = await Todo.findByIdAndDelete(id);
+    if (!deletedTodo) {
+      return NextResponse.json({ message: "Todo not found" }, { status: 404 });
+    }
+
+    return NextResponse.json({
+      message: "Todo deleted successfully",
+      deletedTodo,
+    });
+  } catch (error) {
+    return NextResponse.json(
+      { message: "Error deleting todo", error },
+      { status: 500 }
+    );
+  }
+};
